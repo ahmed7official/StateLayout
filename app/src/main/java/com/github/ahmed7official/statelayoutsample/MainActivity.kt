eea450207e6f22fa4a9460dc4c83a64a98be2f72
Ahmed M Abdalla <ahmed7official@gmail.com>
2020-01-09 16:10:13 +0200

    update

---
+++ app/src/main/java/com/github/ahmed7official/statelayoutsample/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.github.ahmed7official.statelayoutsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
